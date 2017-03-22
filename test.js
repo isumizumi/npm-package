7d90c75fa576b1b983978ea45744d8b9237918c3
isumizumi <isumi.karina72@gmail.com>
2017-03-22 12:00:27 +0700

    Finish: Testing works

---
+++ test.js
@@ -0,0 +1,18 @@
+var in_words = require('./index.js')
+
+console.log(in_words(9));
+console.log(in_words(98));
+console.log(in_words(987));
+console.log(in_words(9876));
+console.log(in_words(98765));
+console.log(in_words(987654));
+console.log(in_words(9876543));
+console.log(in_words(98765432));
+console.log(in_words(987654321));
+console.log(in_words(1987654321));
+console.log(in_words(21987654321));
+console.log(in_words(321987654321));
+console.log(in_words(4321987654321));
+console.log(in_words(54321987654321));
+console.log(in_words(987654321232345));
+console.log(in_words(7000000000000000));
